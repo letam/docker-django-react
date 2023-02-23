6b0302a9c22d38553ad1b12a18c4282b17d33005
Tam Le <tamsgit@gmail.com>
2023-02-23 15:14:59 -0800

    Update old name of project "hello" everywhere else

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hello project.
+URL configuration for helloweb project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
